38403ddc7a9d085fb725339e5e9f359e337658de
ben <544652471@qq.com>
2020-09-10 16:03:32 +0800

    feat: add some feature.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,26 @@
 
 module.exports = {
   extends: "@redchili/eslint-config-airbnb-typescript-prettier",
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  plugins: ['react-hooks'],
   rules: {
     "no-console": 0,
-    camelcase: 2,
+    "camelcase": 0,
+    "import/extensions": "off",
     "react/destructuring-assignment": 1,
     "react/jsx-props-no-spreading": 0,
+    'react-hooks/rules-of-hooks': 'error', // 检查 Hook 的规则
+    'react-hooks/exhaustive-deps': 'warn', // 检查 effect 的依赖
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-member-accessibility": "off",
+    "@typescript-eslint/camelcase": "off",
     "@typescript-eslint/ban-ts-ignore": "off",
+    "@typescript-eslint/no-unused-expressions": 2,
+    "no-unused-expressions": 0,
     "import/prefer-default-export": "off",
     "jsx-a11y/label-has-associated-control": "off",
     "jsx-a11y/click-events-have-key-events": "off",
